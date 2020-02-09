69ac86168f65935da13ecee780ce4d6f6e038220
mustashot <52181098+mustashot@users.noreply.github.com>
2020-02-09 11:02:14 +0100

    demo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  var typed = new Typed(".typed", {
+    strings: ["Together", "We", "Save"],
+    typeSpeed: 200,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
+});
